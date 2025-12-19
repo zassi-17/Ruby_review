04927bd950c4826e504d8ed5aede0c34090c5727
Ubuntu <sgk.k.s.17@gmail.com>
2025-12-19 14:14:57 +0000

    インスタンス演習の復習

--- car.rb
+++ car.rb
@@ -3,7 +3,12 @@ class Car
   def move(distance)
     puts "車は#{distance}キロメートル走ります。"
   end
+
+  def turn(distance)
+    puts "#{distance}へ曲がります。"
+  end
 end
 
 car =Car.new
 car.move(5)
+car.turn("右")
